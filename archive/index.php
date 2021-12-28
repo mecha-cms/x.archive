e73e90b34a2ae19eaa187a789ca4e7371c0c53ac
Taufik Nurrohman <hi@taufik-nurrohman.com>
2021-12-28 20:12:44 +0700

    Update

--- archive/index.php
+++ archive/index.php
@@ -56,7 +56,7 @@ function route($name, $path) {
         'link' => $url . '/' . $path . '/' . $route . '/' . $name
     ]);
     // Set proper parent link
-    $pager->parent = $page;
+    $pager->parent = $i > 0 ? (object) ['link' => $url . '/' . $path . '/' . $route . '/' . $name . '/1'] : $page;
     $pages = $pages->chunk($chunk, $i);
     $GLOBALS['page'] = $page;
     $GLOBALS['pager'] = $pager;
